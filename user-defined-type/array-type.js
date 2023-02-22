52a69cb07991d0425cb4cb6fd385d157178e195e
Akash <na.akash715@gmail.com>
2023-02-22 14:21:29 +0600

    array data type done

--- user-defined-type/array-type.js
+++ user-defined-type/array-type.js
@@ -3,3 +3,6 @@
 var userNames;
 userNames = ['Akash', 'Fahim', "Rakib"];
 console.log(userNames[1]);
+var multipleTypeArray = ['Akash', 'Fahim', "Rakib", 1];
+var multipleTypeArray1 = ['Akash', 'Fahim', "Rakib", 1];
+console.log(userNames.sort());
--- user-defined-type/array-type.ts
+++ user-defined-type/array-type.ts
@@ -5,3 +5,8 @@ let userNames: Array<string>;
 
 userNames = ['Akash', 'Fahim', "Rakib"];
 console.log(userNames[1]);
+
+const multipleTypeArray: Array<string | number> = ['Akash', 'Fahim', "Rakib", 1];
+const multipleTypeArray1: (string | number)[] = ['Akash', 'Fahim', "Rakib", 1];
+
+console.log(userNames.sort());
