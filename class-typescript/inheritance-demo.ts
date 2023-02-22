906b728d575c42a021286a17d508f8442dea4f64
Akash <na.akash715@gmail.com>
2023-02-23 02:31:28 +0600

    feat: complete the inhertance and class usage techniques

---
+++ class-typescript/inheritance-demo.ts
@@ -0,0 +1,32 @@
+export { };
+
+class User {
+    userName: string;
+    age: number;
+
+    constructor(userName: string, age: number) {
+        this.userName = userName;
+        this.age = age;
+    };
+
+    display = (): void => {
+        console.log(`User Name: ${this.userName} & Age: ${this.age}`);
+    };
+}
+
+class Student extends User {
+    studentId: number;
+
+    constructor(userName: string, age: number, studentId: number) {
+        super(userName, age);
+        this.studentId = studentId;
+    };
+
+    display = (): void => {
+        console.log(`User Name: ${this.userName} & Age: ${this.age} & ID: ${this.studentId}`);
+    };
+}
+
+let student1 = new Student("Akash", 23, 1758);
+
+student1.display();
